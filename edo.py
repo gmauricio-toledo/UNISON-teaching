e928193c8db213a2159626242c3b8132dd71b928
gmauricio-toledo <mauricio.toledo@uaem.mx>
2022-10-24 15:18:02 -0700

    Mid point Euler added

--- edo.py
+++ edo.py
@@ -7,7 +7,7 @@ class EulerEDO:
         self.f = f
         self.sol = sol
 
-    def fit(self,a,b,h,x0,y0,heun=False):
+    def fit(self,a,b,h,x0,y0,method='Euler'):
         self.a = a
         self.b = b
         self.h = h
@@ -15,48 +15,18 @@ class EulerEDO:
         xs = np.linspace(a,b,n+1)
         ys = np.zeros_like(xs)
         ys[0] = y0
-        if not heun:
-            for j in range(1,n+1):
-                ys[j] = ys[j-1] + self.f(xs[j-1])*h
-        else:
-            for j in range(1,n+1):
-                ys[j] = ys[j-1] + (self.f(xs[j-1])+self.f(xs[j]))*h/2
-        self.xs = xs
-        self.ys = ys
-    
-    def plot(self,plot_nodes=True):
-        xaxis = np.linspace(self.a,self.b,100)
-        plt.figure(dpi=100)
-        plt.plot(self.xs,self.ys,color='blue',label='Solución aprox')
-        if plot_nodes:
-            plt.scatter(self.xs,self.ys,color='blue')
-        if self.sol is not None:
-            plt.plot(xaxis,[self.sol(x) for x in xaxis],color='red',label='Solución real')
-            plt.legend(loc='best')
-        plt.show()    
-
-
-class EulerEDOxy:
-
-    def __init__(self,f,sol=None):
-        self.f = f
-        self.sol = sol
-
-    def fit(self,a,b,h,x0,y0,heun=False):
-        self.a = a
-        self.b = b
-        self.h = h
-        n = int((b-a)/h)
-        xs = np.linspace(a,b,n+1)
-        ys = np.zeros_like(xs)
-        ys[0] = y0
-        if not heun:
+        if method=='Euler':
             for j in range(1,n+1):
                 ys[j] = ys[j-1] + self.f(xs[j-1],ys[j-1])*h
-        else:
+        elif method=='Heun':
             for j in range(1,n+1):
-                y_pred = ys[j-1] + self.f(xs[j-1],ys[j-1])*h
-                ys[j] = ys[j-1] + (self.f(xs[j-1],ys[j-1])+self.f(xs[j],y_pred))*h/2
+                y_pred = ys[j-1] + self.f(xs[j-1],ys[j-1])*h # Predictor
+                ys[j] = ys[j-1] + (self.f(xs[j-1],ys[j-1])+self.f(xs[j],y_pred))*h/2 # Corrector
+        elif method=='Mid':
+            for j in range(1,n+1):
+                y_pred = ys[j-1] + 0.5*self.f(xs[j-1],ys[j-1])*h # Predictor
+                x = xs[j-1] + (0.5*h)
+                ys[j] = ys[j-1] + self.f(x,y_pred)*h # Corrector
         self.xs = xs
         self.ys = ys
     
@@ -69,6 +39,7 @@ class EulerEDOxy:
         if self.sol is not None:
             plt.plot(xaxis,[self.sol(x) for x in xaxis],color='red',label='Solución real')
             plt.legend(loc='best')
+        plt.xticks(self.xs)
         plt.show()       
 
 
