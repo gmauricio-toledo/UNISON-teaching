6cf31d82099aae5e6c19ba7b1484d202b97b4deb
gmauricio-toledo <mauricio.toledo@uaem.mx>
2022-10-25 09:00:02 -0700

    Runge-Kutta de segundo orden

--- edo.py
+++ edo.py
@@ -40,7 +40,41 @@ class EulerEDO:
             plt.plot(xaxis,[self.sol(x) for x in xaxis],color='red',label='Solución real')
             plt.legend(loc='best')
         plt.xticks(self.xs)
-        plt.show()       
+        plt.show()      
+
+    def table(self):
+        if self.sol is not None:
+            valores_reales = self.sol(self.xs)
+            errores_relativos = (self.sol(self.xs) - self.ys)/self.sol(self.xs)
+            print_table(self.xs,self.ys,valores_reales,errores_relativos)
+        else:
+            print("No hay una solución real") 
+
+
+
+class RungeKutta2(EulerEDO):
+
+    def __init__(self,f,sol=None,a2=0.5):
+         super().__init__(f,sol) 
+         self.a2 = a2
+         self.a1 = 1-a2
+         self.p = 1/(2*a2)
+         self.q = 1/(2*a2)
+
+    def fit(self,a,b,h,x0,y0):
+        self.a = a
+        self.b = b
+        self.h = h
+        n = int((b-a)/h)
+        xs = np.linspace(a,b,n+1)
+        ys = np.zeros_like(xs)
+        ys[0] = y0
+        for j in range(1,n+1):
+            k1 = self.f(xs[j-1],ys[j-1])
+            k2 = self.f(xs[j-1]+self.p*h,ys[j-1]+self.q*k1*h)
+            ys[j] = ys[j-1] + (self.a1*k1 + self.a2*k2)*h 
+        self.xs = xs
+        self.ys = ys
 
 
 def print_table(xs,ys,yrs,ers):
